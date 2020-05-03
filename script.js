00b69a005892ac9067c8912fbead0b76d51cf0d6
Juan <juansanchez721@outlook.com>
2020-05-03 19:57:35 -0400

    Added and displayed appropriate fields

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-$.getJson("https://api.openweathermap.org/data/2.5/weather?q=Orlando&units=imperial&appid=58026d375fc780e495253cb20fdb3452", function(data){
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Orlando&units=imperial&appid=58026d375fc780e495253cb20fdb3452", function(data){
+    console.log(data);
+    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = data.main.temp;
+    var weather = data.weather[0].main;
 
+    // console.log(icon);
+    $('.icon').attr('src', icon);
+    $('.weather').append(weather);
+    $('.temp').append(temp);
 
 });
