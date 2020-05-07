68c842d9eb173b32ab89c263b5325c0e8beeba4e
Juan <juansanchez721@outlook.com>
2020-05-06 22:36:27 -0400

    New work around - started over.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var city = "bronx";
 
-// function myFunction(){
-//     alert("yooo");
- $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial&appid=58026d375fc780e495253cb20fdb3452").then(function(data){
-    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    var temp = data.main.temp;
-    var weather = data.weather[0].main;
 
-    console.log(icon, weather, temp);
-    $('.icon').attr('src', icon);
-    $('.weather').append(weather + " in " + city);
-    $('.temp').append(temp); 
+var button = document.querySelector('.button');
+var city = document.querySelector('.city');
+// var icon = document.querySelector('.icon');
+// var weather = document.querySelector('.weather');
+// var temp = document.querySelector('.temp');
 
+  
+button.addEventListener('click', function(){
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city.value + '&appid=58026d375fc780e495253cb20fdb3452')
+.then(response => response.json())
+.then(data => console.log(data))
+
+.catch(err => console.log("Nah cuz wrond city"))
+    
 });
-// }
